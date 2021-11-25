c396b72f5af1497573d6bedd0372a7384637a6ab
ClevelandSchemer <samjsmith1996@gmail.com>
2021-11-25 05:46:42 -0500

    Added tokenizer for arithmetic strings.

--- code/main.cpp
+++ code/main.cpp
@@ -1,7 +1,125 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(int argc, char **argv)
+#include "sb.h"
+
+typedef int bool32;
+
+enum TokenType
 {
-    printf("Hello, World!");
-    return            0;
+    TOKEN_NUMBER,
+    TOKEN_PLUS,
+    TOKEN_MINUS,
+    TOKEN_STAR,
+    TOKEN_SLASH,
+};
+
+struct Token
+{
+    TokenType type;
+    char *lexeme;
+};
+
+struct Tokenizer
+{
+    char *current;
+    char *start;
+};
+
+inline bool32 is_num(char c)
+{
+    if(c >= '0' && c <= '9')
+    {
+        return true;
+    }
+    return false;
+}
+
+inline bool32 is_white_space(char c)
+{
+    if(c == ' ' || c == '\n' || c == '\t' || c == '\r')
+    {
+        return true;
+    }
+    return false;
+}
+
+inline void eat_white_space(Tokenizer *tokenizer)
+{
+    while(is_white_space(*tokenizer->current))
+    {
+        tokenizer->current++;
+    }
+}
+
+Token get_next_token(Tokenizer *tokenizer)
+{
+    Token result = {};
+
+    if(*tokenizer->current == '+')
+    {
+        result.type = TOKEN_PLUS;
+        tokenizer->current++;
+    }
+    else if(*tokenizer->current == '-')
+    {
+        result.type = TOKEN_MINUS;
+        tokenizer->current++;
+    }
+    else if(*tokenizer->current == '*')
+    {
+        result.type = TOKEN_STAR;
+        tokenizer->current++;
+    }
+    else if(*tokenizer->current == '/')
+    {
+        result.type = TOKEN_SLASH;
+        tokenizer->current++;
+    }
+    else if(is_num(*tokenizer->current))
+    {
+        while(is_num(*tokenizer->current))
+        {
+            tokenizer->current++;
+        }
+        result.type = TOKEN_NUMBER;
+    }
+
+    size_t len = tokenizer->current - tokenizer->start;
+    char *lexeme = (char *) malloc(len + 1);
+    memcpy(lexeme, tokenizer->start, len);
+    lexeme[len] = '\0';
+    result.lexeme = lexeme;
+
+    return result;
+}
+
+Token *tokenize_string(char *string)
+{
+    Token *tokens = NULL;
+    Tokenizer tokenizer = {};
+    tokenizer.start = string;
+    tokenizer.current = tokenizer.start;
+    
+    while(*tokenizer.current != '\0')
+    {
+        Token nextToken = get_next_token(&tokenizer);
+        buf_push(tokens, nextToken);
+        eat_white_space(&tokenizer);
+        tokenizer.start = tokenizer.current;
+    }
+
+    return tokens;
+}
+
+int evaluate(char *string) {
+    Token *tokens = tokenize_string(string);
+	return 0;
+}
+
+int main(int argc, char **argv) {
+	char *test = "3 + 4";
+	int solution = evaluate(test);
+	printf("Hello, World!");
+	return 0;
 }
---
+++ code/sb.h
@@ -0,0 +1,44 @@
+#if !defined(SB_H)
+
+#define SB_H
+
+#include <stdlib.h>
+
+typedef struct {
+	int count;
+	int capacity;
+} buffer_header;
+
+#define buf_header(b) ((buffer_header *)(b)-1)
+#define buf_count_raw(b) buf_header(b)->count
+#define buf_cap_raw(b) buf_header(b)->capacity
+#define buf_count(b) ((b) == NULL ? 0 : buf_count_raw(b))
+#define buf_cap(b) ((b) == NULL ? 0 : buf_cap_raw(b))
+
+#define buf__add(b, n) (buf_count_raw(b) += (n), &(b)[buf_count_raw(b) - (n)])
+#define buf_add(b, n) \
+	(need_grow(b, n) ? buf_grow(b, n), buf__add(b, n) : buf__add(b, n))
+#define buf__push(b, a) ((b)[buf_count_raw(b)++] = (a))
+#define buf_push(b, a) \
+	need_grow(b, 1) ? buf_grow(b, 1), buf__push(b, a) : buf__push(b, a)
+#define need_grow(b, n) \
+	(((b) == NULL) || buf_count(b) + (n) > buf_cap(b) ? 1 : 0)
+#define buf_grow(b, n) grow(buf_cap(b), sizeof(*(b)), (n), (void **)&(b))
+#define buf_free(b) ((b) ? free(buf_header(b)), 0 : 0)
+
+void grow(int capacity, size_t elementSize, int sizeToGrow, void **data) {
+	int newCapacity = capacity ? (capacity * 2) + sizeToGrow : 8 + sizeToGrow;
+
+	void *pointer;
+	if (*data) {
+		pointer = realloc(buf_header(*data),
+											(newCapacity * elementSize) + sizeof(buffer_header));
+	} else {
+		pointer = realloc(0, (newCapacity * elementSize) + sizeof(buffer_header));
+		((buffer_header *)pointer)->count = 0;
+	}
+	*data = (void *)((buffer_header *)pointer + 1);
+	buf_cap_raw(*data) = newCapacity;
+}
+
+#endif
